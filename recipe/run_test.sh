636df0584008947f662a2f55c84eb515cac60ded
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2022-03-08 23:06:45 -0400

    Fix dependencies conflicts

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,14 +2,14 @@
 
 set -ex
 
-export SUPERSET_HOME={{ PREFIX }}  # [unix]
-superset db upgrade
 export FLASK_APP='superset'
-flask fab create-admin
-  --username admin
-  --firstname admin
-  --lastname admin
-  --email admin@fab.org
+
+superset db upgrade
+flask fab create-admin \
+  --username admin \
+  --firstname admin \
+  --lastname admin \
+  --email admin@fab.org \
   --password admin
-superset load_examples
+# superset load_examples
 superset init
